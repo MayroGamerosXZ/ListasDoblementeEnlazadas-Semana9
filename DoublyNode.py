5afa835dd968c47129c87e90b9a3054ef0d0a484
Mayro Geovanni Barrios Gameros <mbarriosg8@miumg.edu.gt>
2025-04-08 22:57:37 -0600

    Delete DoublyNode.py

--- DoublyNode.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-
-# Clase DoublyNode
-class DoublyNode:
-    def __init__(self, value):
-        self.value = value
-        self.prev = None
-        self.next = None
